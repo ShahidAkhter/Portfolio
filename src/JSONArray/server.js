edb48ba7d70695e134c17643e78f580c478eb76a
ShahidAkhter <shahidakhter5661@gmail.com>
2023-05-14 14:30:35 +0530

    Selection Tag

--- src/JSONArray/server.js
+++ src/JSONArray/server.js
@@ -5,10 +5,10 @@ const PORT = 3000; // Replace with your desired port
 
 app.get('/api/myProjects', async (req, res) => {
   try {
-    const googleAppsScriptURL = 'https://script.google.com/macros/s/AKfycbzPZdwvxa6qSDBj4hcMx-Wun7HuWd_nyouuiBCYeGR4dSZ0JpuxIhIn43y0b5lHWu5DJg/exec';
+    const googleAppsScriptURL = 'GoogleDriveLink';
     const response = await axios.get(googleAppsScriptURL);
     res.json(response.data);
-    console.log(esponse.data)
+    console.log(rdesponse.data)
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,12 +4,20 @@ import { projectsArray } from '../JSONArray/projectsArr'
 
 const Projects = (props) => {
     return (
-        <div className="min-h-2">
-            <div className='flex f-center f-wrap'>
-                {projectsArray && projectsArray.map((element, i) => {
-                    // props.updateProgress(0+(projectsArray.length*100))
-                    return <CardItem img={element.image} title={element.title} category={element.category} description={element.description} index={i} key={i} updateView={props.updateView} />
-                })}
+        <div className='w-auto'>
+            <div className='text-right w-93-per'>
+                <select className="categoryType border-1 border-radius text-size-4">
+                    <option value="Web App">All</option>
+                    <option value="Web App">Web App</option>
+                </select>
+            </div>
+            <div className="min-h-2">
+                <div className='flex f-center f-wrap margin-t'>
+                    {projectsArray && projectsArray.map((element, i) => {
+                        // props.updateProgress(0+(projectsArray.length*100))
+                        return <CardItem img={element.image} title={element.title} category={element.category} description={element.description} index={i} key={i} updateView={props.updateView} />
+                    })}
+                </div>
             </div>
         </div>
     )
