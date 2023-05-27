01079fbffea5079e9c4611dc47228a841cf5ebd7
ShahidAkhter <shahidakhter5661@gmail.com>
2023-05-27 17:28:40 +0530

    upadating imgs in cards and view page

--- src/JSONArray/projectsArr.js
+++ src/JSONArray/projectsArr.js
@@ -1,18 +1,18 @@
-import logo from '../logo.jpg';
+import myLinksLogo from '../assets/cover/myLinks.png';
 // import bingoImg from 'https://bingo.shahidakhter.repl.co/favicon.jpg';
-export const projectsArray=[
+export const projectsArray = [
     {
-        image:logo,
-        title:"MyLinks",
-        category:"Web App",
-        metaDescription:"MyLinks is a website where all of the links mostly used by me displays here.",
-        detailedDescription:`<div class="text-size-4 text-color-2 font-bold">Introduction</div>
+        image: myLinksLogo,
+        title: "MyLinks",
+        category: "Web App",
+        metaDescription: "MyLinks is a website where all of the links mostly used by me displays here.",
+        detailedDescription: `<div class="text-size-4 text-color-2 font-bold">Introduction</div>
         <p>myLinks is a website created by me to prevent forgetting important website names or URLs that I frequently use.</p>
         <div class="text-size-4 text-color-2 font-bold">Evolution of myLinks</div>
         <p>The idea for myLinks came to me when I realized that I often forget the names of websites I frequently visit, making it difficult for me to remember them. Therefore, I came up with the concept of creating this website called myLinks.</p>
         <div class="text-size-4 text-color-2 font-bold">Useful Options:</div>
         <p>This website not only allows you to create a collection of links but also provides a selection tag for categorizing them. By changing the selection tag's value, you can display only the links belonging to a specific category on the screen.</p>
         `,
-        visitURL:"https://mylinks.shahidakhter.repl.co/"
+        visitURL: "https://mylinks.shahidakhter.repl.co/"
     },
 ];
--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -12,17 +12,19 @@ const CardItem = (props) => {
   }
   return (
     <div className='cards bg-3 text-justify'>
-      <img src={props.img} className="cardImg" alt='CardImg' />
+      <div className='flex f-center'>
+        <img src={props.img} className="cardImg" alt='CardImg' />
+      </div>
       <div className="m-0">
         <div className="title font-bold text-pink">{props.title}</div>
         <div className="category font-bold text-color-2">{props.category}</div>
         <div className="description h-7">
-          {props.metaDescription.length > maxLength ? <div>{props.metaDescription.slice(0, maxLength)}...<Link className="links ml-0" to="/view"><span className='font-bold ml-0 links text-pink cursor-pointer' onClick={()=>{props.updateView(props.index)}}>Read More</span></Link></div> : props.metaDescription}
+          {props.metaDescription.length > maxLength ? <div>{props.metaDescription.slice(0, maxLength)}...<Link className="links ml-0" to="/view"><span className='font-bold ml-0 links text-pink cursor-pointer' onClick={() => { props.updateView(props.index) }}>Read More</span></Link></div> : props.metaDescription}
         </div>
         <div className="btns btns-1">
           {/* <Link className="links ml-0" to="/Readmore"><button className='bg text-white linkBtns linkBtn1 ml-0'>Read More</button></Link> */}
-          <Link className="links ml-0" to={`/Projects/${replaceInStr(projectsArray[props.index].title, /\s+/g, "_")}_${props.index+1}`}>
-            <button className='bg text-white linkBtns linkBtn1 ml-0' onClick={()=>{props.updateView(props.index)}}>View</button>
+          <Link className="links ml-0" to={`/Projects/${replaceInStr(projectsArray[props.index].title, /\s+/g, "_")}_${props.index + 1}`}>
+            <button className='bg text-white linkBtns linkBtn1 ml-0' onClick={() => { props.updateView(props.index) }}>View</button>
           </Link>
         </div>
       </div>
