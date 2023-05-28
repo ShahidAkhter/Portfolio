5978d73e4acf7ba0686499803070e647a1431c00
ShahidAkhter <shahidakhter5661@gmail.com>
2023-05-28 23:15:43 +0530

    Hiding Blinking Typed Cursor

--- src/components/TypedText.js
+++ src/components/TypedText.js
@@ -9,7 +9,7 @@ const TypedText = ({ strings }) => {
       strings: strings,
       typeSpeed: 35,
       backSpeed: 35,
-      loop: true
+      loop: false
     };
 
     typedRef.current = new Typed('.typed-text', options);
@@ -19,6 +19,13 @@ const TypedText = ({ strings }) => {
       typedRef.current.destroy();
     };
   }, [strings]);
+
+  useEffect(() => {
+    const cursorElement = document.querySelector('.typed-cursor');
+    if (cursorElement) {
+      cursorElement.style.opacity = '0'; // Set opacity to 0 to hide the cursor line
+    }
+  }, []);
 
   return <span className="typed-text" />;
 };
