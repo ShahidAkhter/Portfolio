e2b1a8f38448f0d0b4dfa5d53b7f661c3f7e9160
ShahidAkhter <shahidakhter5661@gmail.com>
2023-05-28 23:16:57 +0530

    Reverting to previous version

--- src/components/TypedText.js
+++ src/components/TypedText.js
@@ -9,7 +9,7 @@ const TypedText = ({ strings }) => {
       strings: strings,
       typeSpeed: 35,
       backSpeed: 35,
-      loop: false
+      loop: true
     };
 
     typedRef.current = new Typed('.typed-text', options);
@@ -19,13 +19,6 @@ const TypedText = ({ strings }) => {
       typedRef.current.destroy();
     };
   }, [strings]);
-
-  useEffect(() => {
-    const cursorElement = document.querySelector('.typed-cursor');
-    if (cursorElement) {
-      cursorElement.style.opacity = '0'; // Set opacity to 0 to hide the cursor line
-    }
-  }, []);
 
   return <span className="typed-text" />;
 };
